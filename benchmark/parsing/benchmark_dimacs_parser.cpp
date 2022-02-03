35e7e8d5b6f28665e4405104e78960e0720f111a
Marc Luqué <marc.luque@outlook.com>
2022-02-03 01:10:27 +0100

    DIMACS Parser: Add some todos in benchmark

--- benchmark/parsing/benchmark_dimacs_parser.cpp
+++ benchmark/parsing/benchmark_dimacs_parser.cpp
@@ -6,7 +6,11 @@
 #include <benchmark/benchmark.h>
 
 static void BM_dimacs_parse_file(benchmark::State& state) {
+    // TODO: Print file size
+
     for (auto _ : state)
         std::string empty_string;
+
+    // TODO: Print parsing stats (i.e., #literals, #clauses)
 }
 BENCHMARK(BM_dimacs_parse_file);
