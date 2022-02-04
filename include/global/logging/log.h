1d3ebc97593161d999a2dd1072fc0efa1960993c
Marc Luqué <marc.luque@outlook.com>
2022-02-04 17:34:46 +0100

    Log: Introduce a log for debugging & error messages

---
+++ include/global/logging/log.h
@@ -0,0 +1,16 @@
+/** @file */
+
+//
+// Created with <3 by marcluque, February 2022
+//
+
+#ifndef YASER_LOG_H
+#define YASER_LOG_H
+
+#include "global/attributes.h"
+
+ATTR_NON_NULL void log_debug(const char* file_path, const char* debug_message);
+
+ATTR_NON_NULL void log_error(const char* file_path, const char* error_message);
+
+#endif //YASER_LOG_H
