8bb99d8cfa3d6275d15005f24db165d4f04361b5
Marc Luqué <marc.luque@outlook.com>
2022-02-04 17:18:34 +0100

    Global: Add some separate cleanup for global allocations

---
+++ src/global/clean_up.c
@@ -0,0 +1,29 @@
+//
+// Created with <3 by marcluque, February 2022
+//
+
+#include "global/cleanup.h"
+#include "assignments/assignment_stack.h"
+#include "assignments/assignment_unit_clauses.h"
+#include "assignments/assignment_map.h"
+#include "watched-literals/clause_literal_map.h"
+#include "watched-literals/literal_clause_map.h"
+#include "global/formula.h"
+#include <stdio.h>
+
+void cleanup_all(void) {
+    // Make sure all messages get out
+    fflush(stdout);
+
+    // assignment
+    assignment_stack_clear();
+    assignment_unit_clause_stack_clear();
+    assignment_map_clear();
+
+    // watched literals
+    clause_literal_map_clear();
+    literal_clause_map_clear();
+
+    // formula
+    formula_clear();
+}
--- src/main.c
+++ src/main.c
@@ -5,27 +5,11 @@
 #include "parsing/dimacs_parser.h"
 #include "watched-literals/watched_literals.h"
 #include "conflicts/dpll.h"
-#include "global/formula.h"
-#include "assignments/assignment_stack.h"
-#include "assignments/assignment_unit_clauses.h"
 #include "assignments/assignment_map.h"
-#include "watched-literals/clause_literal_map.h"
-#include "watched-literals/literal_clause_map.h"
-#include <stdio.h>
-
-static void clean_up(void) {
-    // assignment
-    assignment_stack_clear();
-    assignment_unit_clause_stack_clear();
-    assignment_map_clear();
-
-    // watched literals
-    clause_literal_map_clear();
-    literal_clause_map_clear();
-
-    // formula
-    formula_clear();
-}
+#include "global/cleanup.h"
+#include "global/logging/log.h"
+#include "global/logging/yaser_assert.h"
+#include "global/logging/yaser_malloc.h"
 
 int main(void) {
     dimacs_parse_file("NULL");
@@ -34,7 +18,7 @@ int main(void) {
 
     dpll();
 
-    clean_up();
+    cleanup_all();
 
     return EXIT_SUCCESS;
 }
