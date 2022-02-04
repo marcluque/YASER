87948dfe94587fb886905e4d7f85ed0faea76edf
Marc Luqué <marc.luque@outlook.com>
2022-02-04 17:34:06 +0100

    Global: Add special assertion macro that prints more useful information

---
+++ include/global/logging/log_common.h
@@ -0,0 +1,51 @@
+/** @file */
+
+//
+// Created with <3 by marcluque, February 2022
+//
+
+#ifndef YASER_LOG_COMMON_H
+#define YASER_LOG_COMMON_H
+
+#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+#define COMMON_FORMAT "[%s][YASER - %s][%s]"
+
+// Subtract 1 for NULL-termination
+#define MAX_LEN_BASENAME (64 - 1)
+#define MAX_LEN_TIME_BUFFER (10U)
+
+static char* get_time(char* time_buffer) {
+    strftime(time_buffer, MAX_LEN_TIME_BUFFER, "%H:%M:%S", localtime(&(time_t){time(NULL)}));
+    return time_buffer;
+}
+
+static void get_basename(const char* const file_path, char* const basename_buffer) {
+    size_t len = strlen(file_path);
+    size_t i = len - 1;
+    for (; file_path[i] != '/'; --i);
+
+    len = len - 1 - i;
+    // We don't assert since yaser_assert relies on the log
+    if (len > MAX_LEN_BASENAME) {
+        char time_buffer[MAX_LEN_TIME_BUFFER];
+        get_time(time_buffer);
+        printf(COMMON_FORMAT ":" ANSI_COLOR_RED " basename_buffer (length: %d) too small for basename (length: %lu)\n" ANSI_COLOR_RESET, time_buffer, ANSI_COLOR_RED "ERROR" ANSI_COLOR_RESET, "log_common.h", MAX_LEN_BASENAME, len);
+        return;
+    }
+
+    strncpy(basename_buffer, file_path + i + 1, len);
+}
+
+#endif //YASER_LOG_COMMON_H
---
+++ include/global/logging/yaser_assert.h
@@ -0,0 +1,39 @@
+/** @file */
+
+//
+// Created with <3 by marcluque, February 2022
+//
+
+#ifndef YASER_ASSERT_H
+#define YASER_ASSERT_H
+
+#include "../cleanup.h"
+#include "yaser_malloc.h"
+#include "log_common.h"
+#include <malloc.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_DEBUG_OUTPUT (512U)
+
+#define YASER_ASSERT(expression) asserted(!(expression), __FILE__, __LINE__, #expression)
+
+static void asserted(const int cond, const char* const file_path, const int line, const char* const expression) {
+#if defined(YASER_DEBUG)
+    if (cond) {
+        char* out = malloc(MAX_DEBUG_OUTPUT * sizeof(char));
+        YASER_CHECK_MALLOC(out, __FILE__, __LINE__);
+        char basename[MAX_LEN_BASENAME];
+        get_basename(file_path, basename);
+        char time_buffer[MAX_LEN_TIME_BUFFER];
+        get_time(time_buffer);
+        printf("[%s][YASER - " ANSI_COLOR_YELLOW "ASSERTION" ANSI_COLOR_RESET "][%s:%d]:" ANSI_COLOR_YELLOW " %s does not hold\n" ANSI_COLOR_RESET, time_buffer, basename, line, expression);
+        cleanup_all();
+        exit(EXIT_FAILURE);
+    }
+#else
+    return;
+#endif
+}
+
+#endif //YASER_ASSERT_H
