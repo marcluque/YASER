83605126952a9c55c2d57881b1cee77ba6b4a3ac
Marc Luqué <marc.luque@outlook.com>
2022-02-05 23:43:25 +0100

    Assertions: Update yaser_asser to be able to display values and expression at the same time

--- include/global/logging/yaser_assert.h
+++ include/global/logging/yaser_assert.h
@@ -13,10 +13,11 @@
 #include <malloc.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-#define YASER_ASSERT(expression) asserted(!(expression), __FILE__, __LINE__, #expression)
+#define YASER_ASSERT(left,operator_,right) asserted(!((left) operator_ (right)), (left), #operator_, (right), __FILE__, __LINE__, #left " " #operator_ " " #right)
 
-static void asserted(const int cond, const char* const file_path, const int line, const char* const expression) {
+static void asserted(const bool cond, const long long left, const char* const operator, const long long right, const char* const file_path, const int line, const char* const expression) {
 #if defined(YASER_DEBUG)
   if (cond) {
     char basename[MAX_LEN_BASENAME];
@@ -25,8 +26,8 @@ static void asserted(const int cond, const char* const file_path, const int line
     char time_buffer[MAX_LEN_TIME_BUFFER];
     get_time(time_buffer);
 
-    const char* format = "[%s][YASER - " ANSI_COLOR_YELLOW "ASSERTION" ANSI_COLOR_RESET "][%s:%d]:" ANSI_COLOR_YELLOW " %s does not hold\n" ANSI_COLOR_RESET;
-    printf(format, time_buffer, basename, line, expression);
+    const char* format = "[%s][YASER - " ANSI_COLOR_YELLOW "ASSERTION" ANSI_COLOR_RESET "][%s:%d]:" ANSI_COLOR_YELLOW " \"%s\" does not hold: %d %s %d\n" ANSI_COLOR_RESET;
+    printf(format, time_buffer, basename, line, expression, left, operator, right);
 
     cleanup_all();
     yaser_exit();
