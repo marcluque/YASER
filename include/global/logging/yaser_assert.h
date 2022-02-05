35c5010e24f005f24d02cac2cb9978f40d73ecaa
Marc Luqué <marc.luque@outlook.com>
2022-02-05 22:53:08 +0100

    Assertions: Clean up yaser_assert

--- include/global/logging/yaser_assert.h
+++ include/global/logging/yaser_assert.h
@@ -14,8 +14,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_DEBUG_OUTPUT (512U)
-
 #define YASER_ASSERT(expression) asserted(!(expression), __FILE__, __LINE__, #expression)
 
 static void asserted(const int cond, const char* const file_path, const int line, const char* const expression) {
@@ -23,13 +21,15 @@ static void asserted(const int cond, const char* const file_path, const int line
   if (cond) {
     char basename[MAX_LEN_BASENAME];
     get_basename(file_path, basename);
+
     char time_buffer[MAX_LEN_TIME_BUFFER];
     get_time(time_buffer);
-    printf("[%s][YASER - " ANSI_COLOR_YELLOW "ASSERTION" ANSI_COLOR_RESET "][%s:%d]:" ANSI_COLOR_YELLOW
-           " %s does not hold\n" ANSI_COLOR_RESET,
-           time_buffer, basename, line, expression);
+
+    const char* format = "[%s][YASER - " ANSI_COLOR_YELLOW "ASSERTION" ANSI_COLOR_RESET "][%s:%d]:" ANSI_COLOR_YELLOW " %s does not hold\n" ANSI_COLOR_RESET;
+    printf(format, time_buffer, basename, line, expression);
+
     cleanup_all();
-    exit(EXIT_FAILURE);
+    yaser_exit();
   }
 #else
   return;
