0bbaa70173eff3fec7c03b4a5143615f041226a2
Marc Luqué <marc.luque@outlook.com>
2022-02-05 22:54:01 +0100

    Malloc: Clean up yaser malloc

--- include/global/logging/yaser_malloc.h
+++ include/global/logging/yaser_malloc.h
@@ -8,20 +8,27 @@
 #define YASER_MALLOC_H
 
 #include "log_common.h"
+#include "global/defines.h"
 
-#define YASER_CHECK_MALLOC(address_, file_path_, line_)                                                           \
+#define ERROR_ID ANSI_COLOR_RED "ERROR" ANSI_COLOR_RESET
+
+static void check_malloc(const void* const address, const char* const file_path, const size_t line) {
+  if ((address) == NULL) {
+    char basename_[MAX_LEN_BASENAME];
+    get_basename(file_path, basename_);
+    char time_buffer_[MAX_LEN_TIME_BUFFER];
+    get_time(time_buffer_);
+    printf("[%s][YASER - " ERROR_ID "][%s:%zu]:" ANSI_COLOR_RED
+           " malloc failed\n" ANSI_COLOR_RESET,
+           time_buffer_, basename_, line);
+    cleanup_all();
+    yaser_exit();
+  }
+}
+
+#define YASER_CHECK_MALLOC(address_)                                                                              \
   do {                                                                                                            \
-    if ((address_) == NULL) {                                                                                     \
-      char basename_[MAX_LEN_BASENAME];                                                                           \
-      get_basename(file_path_, basename_);                                                                        \
-      char time_buffer_[MAX_LEN_TIME_BUFFER];                                                                     \
-      get_time(time_buffer_);                                                                                     \
-      printf("[%s][YASER - " ANSI_COLOR_RED "ERROR" ANSI_COLOR_RESET "][%s:%d]:" ANSI_COLOR_RED                   \
-             " malloc failed\n" ANSI_COLOR_RESET,                                                                 \
-             time_buffer_, basename_, line_);                                                                     \
-      cleanup_all();                                                                                              \
-      exit(EXIT_FAILURE);                                                                                         \
-    }                                                                                                             \
+    check_malloc(address_, __FILE__, __LINE__);                                                                                                           \
   } while (0)
 
 #endif // YASER_MALLOC_H
