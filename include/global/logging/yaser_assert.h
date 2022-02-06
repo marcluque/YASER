377b33d367aefdc98b03d3524ebfdded5e4d4059
Marc Luqué <marc.luque@outlook.com>
2022-02-07 00:04:25 +0100

    Assertions: Make the YASER_ASSERT a macro, since that allows for better IDE code analysis

--- include/global/logging/yaser_assert.h
+++ include/global/logging/yaser_assert.h
@@ -15,26 +15,26 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define YASER_ASSERT(left,operator_,right) asserted(!(((long long) left) operator_ ((long long) right)), ((long long) left), #operator_, ((long long) right), __FILE__, __LINE__, #left " " #operator_ " " #right)
-
-static void asserted(const bool cond, const long long left, const char* const operator, const long long right, const char* const file_path, const int line, const char* const expression) {
 #if defined(YASER_DEBUG)
-  if (cond) {
-    char basename[MAX_LEN_BASENAME];
-    get_basename(file_path, basename);
-
-    char time_buffer[MAX_LEN_TIME_BUFFER];
-    get_time(time_buffer);
-
-    const char* format = "[%s][YASER - " ANSI_COLOR_YELLOW "ASSERTION" ANSI_COLOR_RESET "][%s:%d]:" ANSI_COLOR_YELLOW " \"%s\" does not hold: %d %s %d\n" ANSI_COLOR_RESET;
-    printf(format, time_buffer, basename, line, expression, left, operator, right);
-
-    cleanup_all();
-    yaser_exit();
-  }
+#define YASER_ASSERT(left,operator_,right) \
+do {                                       \
+  if (!(left operator_ right)) { \
+    char basename[MAX_LEN_BASENAME]; \
+    get_basename(__FILE__, basename); \
+    \
+    char time_buffer[MAX_LEN_TIME_BUFFER]; \
+    get_time(time_buffer); \
+    \
+    const char* format = "[%s][YASER - " ANSI_COLOR_YELLOW "ASSERTION" ANSI_COLOR_RESET "][%s:%d]:" \
+    ANSI_COLOR_YELLOW " \"%s\" does not hold: %d %s %d\n" ANSI_COLOR_RESET; \
+    printf(format, time_buffer, basename, __LINE__, #left " " #operator_ " " #right, left, #operator_, right); \
+     \
+    cleanup_all(); \
+    yaser_exit(); \
+  }                 \
+} while (0)
 #else
-  return;
+    #define YASER_ASSERT(left,operator_,right) ;
 #endif
-}
 
 #endif // YASER_ASSERT_H
