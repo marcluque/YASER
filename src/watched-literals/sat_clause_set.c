de35447fdb36cf1ee78f80cca93e47ae93b8dfe4
Marc Luqué <marc.luque@outlook.com>
2022-02-08 22:27:41 +0100

    Watched Literals: Introduce set to store satisfied clauses

--- src/watched-literals/sat_clause_set.c
+++ src/watched-literals/sat_clause_set.c
@@ -6,16 +6,24 @@
 #include "sat_clause_set.h"
 #include "global/defines.h"
 #include "global/formula.h"
+#include "uthash.h"
+
+static SatClauseItem* sat_clause_set = NULL;
 
 // TODO: Maintain count of sat clauses to determine whether there's unsat clauses left
 static clause_index clause_count;
 
 bool sat_clause_set_contains(clause_index clause) {
-  return true;
+  SatClauseItem* item;
+  HASH_FIND(hh, sat_clause_set, &clause, sizeof(clause_index), item);
+
+  return item != NULL;
 }
 
 void sat_clause_set_add(clause_index clause) {
+  // TODO: Make sure that clause is not already in set!!
 
+  clause_count++;
 }
 
 bool sat_clause_set_exists_unsat(void) {
--- src/watched-literals/sat_clause_set.h
+++ src/watched-literals/sat_clause_set.h
@@ -1,3 +1,5 @@
+/** @file */
+
 //
 // Created with <3 by marcluque, February 2022
 //
@@ -5,13 +7,26 @@
 #ifndef YASER_SAT_CLAUSE_SET_H
 #define YASER_SAT_CLAUSE_SET_H
 
-#include <stdbool.h>
 #include "global/defines.h"
+#include "uthash.h"
+#include <stdbool.h>
 
-bool sat_clause_set_contains(clause_index clause);
+typedef struct {
+  /**
+   *
+   */
+  clause_index clause;
+
+  /**
+   *
+   */
+  UT_hash_handle hh;
+} SatClauseItem;
+
+ATTR_CONST bool sat_clause_set_contains(clause_index clause);
 
 void sat_clause_set_add(clause_index clause);
 
-bool sat_clause_set_exists_unsat(void);
+ATTR_PURE bool sat_clause_set_exists_unsat(void);
 
 #endif // YASER_SAT_CLAUSE_SET_H
