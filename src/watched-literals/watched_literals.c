8209aaf555f8a989f884ec2581f84d5758deb73a
Marc Luqué <marc.luque@outlook.com>
2022-02-08 22:16:18 +0100

    Watched Literals: Make a great rework of how watched literals are processed with all the new maps/sets/stacks

--- src/watched-literals/watched_literals.c
+++ src/watched-literals/watched_literals.c
@@ -5,74 +5,102 @@
 #include "watched-literals/watched_literals.h"
 #include "global/formula.h"
 #include "conflicts/conflict_resolution.h"
-#include "assignments/assignment_map.h"
 #include "assignments/assignment_sat_clauses.h"
-#include "assignments/assignment_unit_clauses.h"
-#include "watched-literals/literal_clause_map.h"
+#include "assignments/unit_clause_stack.h"
 #include "watched-literals/clause_literal_map.h"
-#include <assert.h>
+#include "global/logging/yaser_assert.h"
+#include "watched_literal_clause_map.h"
+#include "global/logging/log.h"
+#include "sat_clause_set.h"
+#include <limits.h>
 
-ATTR_PURE static formula_pos find_new_literal(const clause_index clause) {
-  assert(clause < NOT_FOUND);
-  assert(clause <= num_clauses);
+#define NOT_FOUND_IN_CLAUSE (INT_MAX)
+#define CLAUSE_ALREADY_SAT (0U)
 
-  for (formula_pos i = 0; i < clauses[clause + 1]; ++i) {
-    value assignment = assignment_map_get_value(clauses[clause] + i);
-    assert(assignment != VALUE_INVALID);
+ATTR_PURE static literal find_new_literal(const clause_index clause, const literal partner_literal) {
+  YASER_ASSERT(clause, <, NOT_FOUND);
+  YASER_ASSERT(clause, <, num_clauses);
 
-    // TODO: Is this correct?
-    if (assignment == 1 || assignment == 0) {
-      return clauses[clause] + i;
+  for (formula_pos i = clauses[clause]; i < clauses[clause + 1]; ++i) {
+    value assignment = assignment_map_get(formula[clauses[clause] + i]);
+
+    if (partner_literal != formula[clauses[clause] + i] && assignment == VALUE_UNASSIGNED) {
+      return formula[clauses[clause] + i];
+    } else if (assignment == VALUE_TRUE) {
+      return CLAUSE_ALREADY_SAT;
     }
   }
 
-  return NOT_FOUND;
+  return NOT_FOUND_IN_CLAUSE;
 }
 
 void watched_literals_init(void) {
   for (clause_index i = 0; i < num_clauses; ++i) {
-    literal_clause_map_add(clauses[i], i);
-    literal_clause_map_add(clauses[i] + 1, i);
+    // If the clause has one literal, we skip it since preprocessing (input file parsing) takes care of it
+    if (clauses[i + 1] - clauses[i] > 1) {
+      // The function takes care of collecting clauses for equal literals (e.g., l_1 -> {c_1, c_2})
+      watched_literal_clause_map_add(formula[clauses[i]], i);
+      watched_literal_clause_map_add(formula[clauses[i]] + 1, i);
+    }
   }
 }
 
-void watched_literals_check(const formula_pos watched_literal_pos) {
-  assert(watched_literal_pos < NOT_FOUND);
-  assert(watched_literal_pos < num_variables);
+static void check_watched_literal_partner(const clause_index clause, const literal negated_watched_literal) {
+  YASER_ASSERT(clause, <, NOT_FOUND);
+  YASER_ASSERT(clause, <, num_clauses);
+  YASER_ASSERT(negated_watched_literal, !=, 0);
+  YASER_ASSERT(negated_watched_literal, !=, INT_MAX);
 
-  // Find clause of negated watched_literal_pos
-  clause_index clause = literal_clause_map_find(watched_literal_pos);
-  if (clause == NOT_FOUND) {
-    // TODO: Print something I guess
-    return;
-  }
+  // Find partner literal of negated_watched_literal
+  literal* watched_literals = clause_literal_map_get(clause);
+  literal partner_literal = watched_literals[0] == negated_watched_literal ? watched_literals[1] : watched_literals[0];
 
-  formula_pos partner_literal_pos = clause_literal_map_find(clause, watched_literal_pos);
-  assert(partner_literal_pos != NOT_FOUND);
-
-  value partner_literal_assignment = assignment_map_get_value(partner_literal_pos);
-
-  formula_pos new_literal_pos;
-  if (partner_literal_assignment == VALUE_TRUE) {
+  literal new_literal;
+  if (assignment_map_get(partner_literal) == VALUE_TRUE) {
     // clause satisfied
-    assignment_sat_clauses_add_clause();
-  } else if ((new_literal_pos = find_new_literal(clause)) != NOT_FOUND) {
-    // Update clause_map
-    clause_literal_map_delete(clause);
-    // TODO: Does an anonymous array work?
-    const formula_pos watched_literals[] = {partner_literal_pos, new_literal_pos};
-    clause_literal_map_add(clause, watched_literals);
+    sat_clause_set_add(clause);
+  } else if ((new_literal = find_new_literal(clause, partner_literal)) != NOT_FOUND_IN_CLAUSE) {
+    // Try to find a new literal to watch
 
-    // Update watched_literal_map
-    literal_clause_map_delete(watched_literal_pos);
-    literal_clause_map_add(new_literal_pos, clause);
-  } else if (partner_literal_assignment == VALUE_INVALID) {
+    // If the new literal happens to be satisfied, we are done
+    if (new_literal == CLAUSE_ALREADY_SAT) {
+      sat_clause_set_add(clause);
+      return;
+    }
+
+    // Update clause_literal_map by simply updating the watched_literals
+    watched_literals[0] = new_literal;
+    watched_literals[1] = partner_literal;
+
+    // Update watched_literal_clause_map:
+    // The call from dpll takes care of removing the required clause for the original watched literal (not the negated one)
+    // We need to make sure that new_literal is mapped to its new clause
+    // partner_literal remains a watched literal for the clause, so no need to clean up
+    watched_literal_clause_map_add(new_literal, clause);
+  } else if (assignment_map_get(partner_literal) == VALUE_UNASSIGNED) {
     // Clause is unit, partner_literal is unassigned
-    assignment_unit_clause_stack_push(partner_literal_pos);
-  } else if (partner_literal_assignment == VALUE_FALSE) {
+    unit_clause_stack_push(clause, partner_literal);
+  } else if (assignment_map_get(partner_literal) == VALUE_FALSE) {
     // Clause is conflicting -> resolve
     conflict_present = true;
     // get variable from assignment stack
     // AssignmentStackItem* assignment = assignment_stack_pop();
+  }
+}
+
+void watched_literals_check(const literal watched_literal) {
+  YASER_ASSERT(watched_literal, !=, 0);
+  YASER_ASSERT(watched_literal, !=, INT_MAX);
+
+  // Find all clauses that contain the negated watched literal (i.e., formula[watched_literal])
+  literal negated_watched_literal = -watched_literal;
+  ClauseArray clause_array = watched_literal_clause_map_get(negated_watched_literal);
+  YASER_ASSERT(clause_array, !=, NULL);
+
+  clause_index* clause = (clause_index*) utarray_front(clause_array);
+  for(; clause != NULL; clause = (clause_index*) utarray_next(clause_array, clause)) {
+    if (!sat_clause_set_contains(*clause)) {
+      check_watched_literal_partner(*clause, negated_watched_literal);
+    }
   }
 }
