72efcc9bff58d154a256e6052acb71d5ed7def25
Marc Luqué <marc.luque@outlook.com>
2022-02-08 21:37:48 +0100

    Watched Literals: Add hash table for satisfied clauses

---
+++ src/watched-literals/satisfied_clauses_set.h
@@ -0,0 +1,15 @@
+//
+// Created with <3 by marcluque, February 2022
+//
+
+#ifndef YASER_SATISFIED_CLAUSES_SET_H
+#define YASER_SATISFIED_CLAUSES_SET_H
+
+#include <stdbool.h>
+#include "global/defines.h"
+
+bool satisfied_clauses_set_is_sat(clause_index clause);
+
+
+
+#endif // YASER_SATISFIED_CLAUSES_SET_H
