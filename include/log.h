0be2647e4231c19bfa197b1d0103ff1e0cc21b26
Marc Luqué <marc.luque@outlook.com>
2023-07-03 22:27:09 +0200

    Add simple logging helpers

--- include/log.h
+++ include/log.h
@@ -1,12 +1,19 @@
 #ifndef YASER_LOG_H
 #define YASER_LOG_H
 
+#include <filesystem>
+#include <string>
 #include <fmt/core.h>
 
 void vlog(const char* log_level, const char* file, int line, fmt::string_view format, fmt::format_args args) {
   fmt::print("[{:5}]: ", log_level);
   fmt::vprint(format, args);
+#ifdef PIPELINE
+  std::filesystem::path p(file);
+  fmt::print(" ({}:{})\n", p.filename(), line);
+#else
   fmt::print(" ({}:{})\n", file, line);
+#endif
 }
 
 template <typename... T>
@@ -15,9 +22,11 @@ void log(const char* log_level, const char* file, int line, fmt::format_string<T
 }
 
 #ifdef YASER_DEBUG
-  #define DEBUG_LOG(format, ...) log("DEBUG", __FILE__, __LINE__, format, __VA_ARGS__)
+  #define DEBUG_LOG(format, ...) log("DEBUG", __FILE__, __LINE__, format __VA_OPT__(,) __VA_ARGS__)
 #else
-  #define log_debug(...) (void) 0;
+  #define DEBUG_LOG(format, ...) (void) 0;
 #endif
+
+#define INFO_LOG(format, ...) log("INFO", __FILE__, __LINE__, format __VA_OPT__(,) __VA_ARGS__)
 
 #endif // YASER_LOG_H
