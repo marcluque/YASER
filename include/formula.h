919d6765c91e48ccb3538dc7118fb93a02acfa8e
Marc Luqué <marc.luque@outlook.com>
2023-07-03 22:26:01 +0200

    Collect ideas for formula representation

--- include/formula.h
+++ include/formula.h
@@ -1,38 +1,71 @@
-//
-// Created by marcluque on 02.07.23.
-//
-
 #ifndef YASER_FORMULA_H
 #define YASER_FORMULA_H
+
+#include <vector>
+#include <cstdint>
 
 /**
  * Only use unsigned int for literals.
  * The problem here is that DIMACS uses negative numbers to represent negated variables, e.g., -3 is !x_3.
  * To circumvent this when parsing one could parse it as signed integer, check whether it is in range
- * [-2^31, 2^31 - 1], and use the LSB to indicate a variable's negation.
+ * [-2^30, 2^30 - 1], and use the LSB to indicate a variable's negation.
  * A literal's assignment could then be queried as follows: assignment_map[formula[i] & 0]
  */
 
 /**
- * Formula CNF:    (-3 5 3) (-1 2 -4) (1 2)
+ * CNF Formula:    (-3 5 3) (-1 2 -4) (1 2)
  *                 |        |         |
  * Clause indices: 0=c1     1=c2      2=c3
  */
 
 /**
- * - Store literals as vector(?)
- * - Access literals (e.g., [])
+ * - Store literals as vector
+ * - Access literals
  * - Iterate over literals (with std::ranges?)
  *
  * - Store start indices (or iterators?) of clauses
- * - Iterate over clauses (with std::ranges?) -> should this be in the same class
+ * - Iterate over clauses (with std::ranges?) -> should this be in the same class?
  *
- * - store, access, and set literal assignments? e.g.: formula.value(i) // = assignment_map[formula[i] & 0]
+ * - Add new clauses as part of clause learning
+ *
+ * - Store, access, and set literal assignments? e.g.: formula.value(i) // = assignment_map[formula[i] & 0]
+ *
+ * - Watched literals map: unordered_map<literal, vector<Clause>> (i.e., a literal points to the clauses its being watched in)
+ *
+ * - Unit clause set: unordered_set<Clause>
+ *
+ * - SAT clause set: unordered_set<Clause>
  */
+using Literal = uint32_t;
+
+using Clause = std::size_t;
+
 class Formula {
 public:
-private:
+  Formula(std::size_t num_variables, std::size_t num_clauses);
 
+  /**
+   * Literals are not changed, hence const and no reference returned.
+   *
+   * @param i
+   * @return
+   */
+  [[nodiscard]] Literal literal(int i) const {
+    return literals[i];
+  }
+
+private:
+  /**
+   * We can have up to 2^31 variables.
+   */
+  std::vector<Literal> literals;
+
+  /**
+   * Stores indices to positions in the ::literals vector.
+   */
+  std::vector<Clause> clauses;
 };
+
+enum class Value { UNASSIGNED = 0, FALSE, TRUE };
 
 #endif // YASER_FORMULA_H
--- src/formula.cpp
+++ src/formula.cpp
@@ -1,1 +1,5 @@
 #include "formula.h"
+
+Formula::Formula(std::size_t num_variables, std::size_t num_clauses)
+    : literals(num_variables * num_clauses), clauses(num_clauses) {}
+
