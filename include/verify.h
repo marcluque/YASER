cb41c123908ddc405c9608469313fda51ae3313b
Marc Luqué <marc.luque@outlook.com>
2023-07-09 18:27:06 +0200

    Experiment with new verify statement

--- include/verify.h
+++ include/verify.h
@@ -1,20 +1,23 @@
 #ifndef YASER_VERIFY_H
 #define YASER_VERIFY_H
 
+#include <functional>
 #include "log.h"
 
+template <typename T, typename Comparator>
+void verify(T lhs, Comparator comparator, T rhs, const char* lhs_str, const char* comparator_str, const char* rhs_str) {
+  if (!comparator(lhs, rhs)) {
+    DEBUG_LOG("{} {} {}", lhs_str, comparator_str, rhs_str);
+    fflush(stdout);
+    fflush(stderr);
+    __builtin_trap();
+  } else {
+    (void) 0;
+  }
+}
+
 #ifdef YASER_DEBUG
-  #define VERIFY(expr) \
-    do {\
-      if (__builtin_expect(!(expr), 0)) {                                                                              \
-        DEBUG_LOG("VERIFICATION FAILED: {}", #expr);                   \
-        fflush(stdout); \
-        fflush(stderr); \
-        __builtin_trap();                                                                                                                    \
-      } else {                                                                                                          \
-        (void) 0;                                                                                                                    \
-      } \
-    } while (0)
+  #define VERIFY(lhs_, comparator_, rhs_) verify(lhs_, comparator_, rhs_, #lhs_, #comparator_, #rhs_)
 #else
   #define VERIFY(expr) (void) 0;
 #endif
