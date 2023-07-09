fe2a38282762d0dacdcf494115a604fd40063448
Marc Luqué <marc.luque@outlook.com>
2023-07-09 18:26:05 +0200

    Add basic structure of DIMACS parser

--- src/main.cpp
+++ src/main.cpp
@@ -5,24 +5,18 @@
 #include "log.h"
 #include "verify.h"
 #include "formula.h"
+#include "dimacs_parser.h"
 
 int main(int argc, char** argv) {
-  VERIFY(0 == 0);
-  DEBUG_LOG("test log {} and {}", 42, fmt::join(std::vector{1, 2, 3}, ","));
-
-  // Read DIMACS file header p cnf <variables> <clauses>
-  // Reserve <variables> * <clauses> for the number of literals?
-  size_t num_variables = 0;
-  size_t num_clauses = 0;
-
-  // Allocates space for literals and clauses
-  Formula formula(num_variables, num_clauses);
+  //VERIFY(1, std::greater<>(), 10);
+  //DEBUG_LOG("test log {} and {}", 42, fmt::join(std::vector{1, 2, 3}, ","));
 
   // DIMACS parser can now fill literal and clause vectors (should use push_back since we might need to reallocate)
   // If we overestimated the number of literals (i.e., #vars * #clauses), there is no issue since we likely need
   // more space for clause learning
 
-  //DimacsParser::parse_formula(input_file, formula)
+  DEBUG_LOG("{}", argv[1]);
+  Formula f = DimacsParser::parse_formula(std::filesystem::path(argv[1]));
 
   INFO_LOG("Parsed input file, took {} ms", 1);
 
