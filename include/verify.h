64d4821a04e1f613253076b3c83d3fdd5b998121
Marc Luqué <marc.luque@outlook.com>
2024-01-01 13:47:26 +0100

    Simplify verify macro

--- include/verify.h
+++ include/verify.h
@@ -5,23 +5,22 @@
 #include "log.h"
 
 template <typename T, typename Comparator>
-void verify(T lhs, Comparator comparator, T rhs, const char* lhs_str, const char* comparator_str, const char* rhs_str) {
-  if (!comparator(lhs, rhs)) {
-    DEBUG_LOG("{} {} {}", lhs_str, comparator_str, rhs_str);
-    fflush(stdout);
-    fflush(stderr);
-    __builtin_trap();
-  } else {
-    (void) 0;
-  }
+void verify(T lhs, Comparator comparator, T rhs, const char* lhs_str, const char* comparator_str,
+            const char* rhs_str) {
+    if (!comparator(lhs, rhs)) {
+        DEBUG_LOG("ASSERT FAILED: {} {} {}", lhs_str, comparator_str, rhs_str);
+        fflush(stdout);
+        fflush(stderr);
+        __builtin_trap();
+    }
 }
 
 #ifdef YASER_DEBUG
-  #define VERIFY(lhs_, comparator_, rhs_) verify(lhs_, comparator_, rhs_, #lhs_, #comparator_, #rhs_)
+    #define VERIFY(lhs_, comparator_, rhs_) verify(lhs_, comparator_, rhs_, #lhs_, #comparator_, #rhs_)
 #else
-  #define VERIFY(expr) (void) 0;
+    #define VERIFY(lhs_, comparator_, rhs_) (void) 0;
 #endif
 
-#define VERIFY_NOT_REACHED() VERIFY(false)
+#define VERIFY_NOT_REACHED() VERIFY(false, std::equals_to<>{}, true)
 
 #endif // YASER_VERIFY_H
