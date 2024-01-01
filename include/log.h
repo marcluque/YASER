b1e820d4e1dd022db86b4413ccac220135e67973
Marc Luqué <marc.luque@outlook.com>
2024-01-01 13:47:00 +0100

    Add warning log

--- include/log.h
+++ include/log.h
@@ -1,12 +1,10 @@
 #ifndef YASER_LOG_H
 #define YASER_LOG_H
 
-#include <filesystem>
-#include <string>
 #include <fmt/core.h>
 
 inline void vlog(const char* log_level, const char* file, int line, fmt::string_view format, fmt::format_args args) {
-  fmt::print("[{:5}]: ", log_level);
+  fmt::print("[{:7}]: ", log_level);
   fmt::vprint(format, args);
 #ifdef PIPELINE
   std::filesystem::path p(file);
@@ -28,6 +26,8 @@ void log(const char* log_level, const char* file, int line, fmt::format_string<T
 #endif
 
 #define INFO_LOG(format, ...) log("INFO", __FILE__, __LINE__, format __VA_OPT__(,) __VA_ARGS__)
+
+#define WARNING_LOG(format, ...) log("WARNING", __FILE__, __LINE__, format __VA_OPT__(,) __VA_ARGS__)
 
 #define ERROR_LOG(format, ...) log("ERROR", __FILE__, __LINE__, format __VA_OPT__(,) __VA_ARGS__)
 
