acdd6e8a6ee13bd00470c7027d06d9627d19e6b4
Marc Luqué <marc.luque@outlook.com>
2025-05-10 01:13:07 +0200

    Add better assert logging

--- include/verify.h
+++ include/verify.h
@@ -4,10 +4,35 @@
 #include "log.h"
 
 #ifdef YASER_DEBUG
+
+template <typename LHS, typename RHS>
+std::enable_if_t<fmt::is_formattable<LHS>() && fmt::is_formattable<RHS>(), void>
+log_assert_failed(const char* lhs_string, const LHS& lhs, const char* comparator, const char* rhs_string, const RHS& rhs) {
+    DEBUG_LOG("ASSERT FAILED: {}(={}) {} {}(={})", lhs_string, lhs, comparator, rhs_string, rhs);
+}
+
+template <typename LHS, typename RHS>
+std::enable_if_t<!fmt::is_formattable<LHS>() && fmt::is_formattable<RHS>(), void>
+log_assert_failed([[maybe_unused]] const char* _lhs_string, const LHS& lhs, const char* comparator, const char* rhs_string, const RHS& rhs) {
+    DEBUG_LOG("ASSERT FAILED: {} {} {}(={})", lhs, comparator, rhs_string, rhs);
+}
+
+template <typename LHS, typename RHS>
+std::enable_if_t<fmt::is_formattable<LHS>() && !fmt::is_formattable<RHS>(), void>
+log_assert_failed(const char* lhs_string, const LHS& lhs, const char* comparator, [[maybe_unused]] const char* _rhs_string, const RHS& rhs) {
+    DEBUG_LOG("ASSERT FAILED: {}(={}) {} {}", lhs_string, lhs, comparator, rhs);
+}
+
+template <typename LHS, typename RHS>
+std::enable_if_t<!fmt::is_formattable<LHS>() && !fmt::is_formattable<RHS>(), void>
+log_assert_failed([[maybe_unused]] const char* _lhs_string, const LHS& lhs, const char* comparator, [[maybe_unused]] const char* _rhs_string, const RHS& rhs) {
+    DEBUG_LOG("ASSERT FAILED: {} {} {}", lhs, comparator, rhs);
+}
+
     #define VERIFY(lhs_, comparator_, rhs_)                                                                       \
         do {                                                                                                      \
             if (!comparator_(lhs_, rhs_)) {                                                                       \
-                DEBUG_LOG("ASSERT FAILED: {} {} {}", #lhs_, #comparator_, #rhs_);                                 \
+                log_assert_failed(#lhs_, lhs_, #comparator_, #rhs_, rhs_);                                        \
                 fflush(stdout);                                                                                   \
                 fflush(stderr);                                                                                   \
                 __builtin_trap();                                                                                 \
