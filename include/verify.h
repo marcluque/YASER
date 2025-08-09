5a32ddc05e36537a1157487595adbf00d8827e03
Marc Luqué <marc.luque@outlook.com>
2025-08-10 00:09:13 +0200

    Add better asserts

--- include/verify.h
+++ include/verify.h
@@ -7,32 +7,32 @@
 
 template <typename LHS, typename RHS>
 std::enable_if_t<fmt::is_formattable<LHS>() && fmt::is_formattable<RHS>(), void>
-log_assert_failed(const char* lhs_string, const LHS& lhs, const char* comparator, const char* rhs_string, const RHS& rhs) {
-    DEBUG_LOG("ASSERT FAILED: {}(={}) {} {}(={})", lhs_string, lhs, comparator, rhs_string, rhs);
+log_assert_failed(const char* file, const int line, const char* lhs_string, const LHS& lhs, const char* comparator, const char* rhs_string, const RHS& rhs) {
+    log("DEBUG", file, line, "ASSERT FAILED: {}(={}) {} {}(={})", lhs_string, lhs, comparator, rhs_string, rhs);
 }
 
 template <typename LHS, typename RHS>
 std::enable_if_t<!fmt::is_formattable<LHS>() && fmt::is_formattable<RHS>(), void>
-log_assert_failed([[maybe_unused]] const char* _lhs_string, const LHS& lhs, const char* comparator, const char* rhs_string, const RHS& rhs) {
-    DEBUG_LOG("ASSERT FAILED: {} {} {}(={})", lhs, comparator, rhs_string, rhs);
+log_assert_failed(const char* file, const int line, const char* _lhs_string, [[maybe_unused]]  const LHS& lhs, const char* comparator, const char* rhs_string, const RHS& rhs) {
+    log("DEBUG", file, line, "ASSERT FAILED: {} {} {}(={})", _lhs_string, comparator, rhs_string, rhs);
 }
 
 template <typename LHS, typename RHS>
 std::enable_if_t<fmt::is_formattable<LHS>() && !fmt::is_formattable<RHS>(), void>
-log_assert_failed(const char* lhs_string, const LHS& lhs, const char* comparator, [[maybe_unused]] const char* _rhs_string, const RHS& rhs) {
-    DEBUG_LOG("ASSERT FAILED: {}(={}) {} {}", lhs_string, lhs, comparator, rhs);
+log_assert_failed(const char* file, const int line, const char* lhs_string, const LHS& lhs, const char* comparator, const char* _rhs_string, [[maybe_unused]]  const RHS& rhs) {
+    log("DEBUG", file, line, "ASSERT FAILED: {}(={}) {} {}", lhs_string, lhs, comparator, _rhs_string);
 }
 
 template <typename LHS, typename RHS>
 std::enable_if_t<!fmt::is_formattable<LHS>() && !fmt::is_formattable<RHS>(), void>
-log_assert_failed([[maybe_unused]] const char* _lhs_string, const LHS& lhs, const char* comparator, [[maybe_unused]] const char* _rhs_string, const RHS& rhs) {
-    DEBUG_LOG("ASSERT FAILED: {} {} {}", lhs, comparator, rhs);
+log_assert_failed(const char* file, const int line, const char* _lhs_string, [[maybe_unused]] const LHS& lhs, const char* comparator, const char* _rhs_string, [[maybe_unused]]  const RHS& rhs) {
+    log("DEBUG", file, line, "ASSERT FAILED: {} {} {}", _lhs_string, comparator, _rhs_string);
 }
 
     #define VERIFY(lhs_, comparator_, rhs_)                                                                       \
         do {                                                                                                      \
             if (!comparator_(lhs_, rhs_)) {                                                                       \
-                log_assert_failed(#lhs_, lhs_, #comparator_, #rhs_, rhs_);                                        \
+                log_assert_failed(__FILE__, __LINE__, #lhs_, lhs_, #comparator_, #rhs_, rhs_);                    \
                 fflush(stdout);                                                                                   \
                 fflush(stderr);                                                                                   \
                 __builtin_trap();                                                                                 \
