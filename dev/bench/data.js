5991c1fcf7c97f6e2c9af45d1c12b8839b8d6114
github-action-benchmark <github@users.noreply.github.com>
2025-09-08 20:31:13 +0000

    add Benchmark (googlecpp) benchmark result for e01f2308a7e58faf120e4086258a9b6727247121

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1757298477743,
+  "lastUpdate": 1757363473657,
   "repoUrl": "https://github.com/marcluque/YASER",
   "entries": {
     "Benchmark": [
@@ -66,6 +66,72 @@ window.BENCHMARK_DATA = {
             "value": 1.9974374017093954,
             "unit": "ms/iter",
             "extra": "iterations: 351\ncpu: 1.997152259259259 ms\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "marc.luque@outlook.com",
+            "name": "Marc Luqué",
+            "username": "marcluque"
+          },
+          "committer": {
+            "email": "marc.luque@outlook.com",
+            "name": "Marc Luqué",
+            "username": "marcluque"
+          },
+          "distinct": true,
+          "id": "e01f2308a7e58faf120e4086258a9b6727247121",
+          "message": "Improve parser performance by removing std::optional usage",
+          "timestamp": "2025-09-08T22:30:19+02:00",
+          "tree_id": "8ee24daa39585e7d655d0534854e1fb892d25980",
+          "url": "https://github.com/marcluque/YASER/commit/e01f2308a7e58faf120e4086258a9b6727247121"
+        },
+        "date": 1757363472540,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_SmallResolution",
+            "value": 283.9577171957841,
+            "unit": "ns/iter",
+            "extra": "iterations: 2354385\ncpu: 283.9439361022093 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_BigResolution",
+            "value": 0.060338077433084676,
+            "unit": "ms/iter",
+            "extra": "iterations: 12217\ncpu: 0.06033292625030696 ms\nthreads: 1"
+          },
+          {
+            "name": "BM_ParseSmallFormula/small_formula",
+            "value": 973.4057103563678,
+            "unit": "ns/iter",
+            "extra": "iterations: 693687\ncpu: 973.3408684320164 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_ParseSmallFormula/big_formula",
+            "value": 1.418776791411008,
+            "unit": "ms/iter",
+            "extra": "iterations: 489\ncpu: 1.4186899222903886 ms\nthreads: 1"
+          },
+          {
+            "name": "BM_PigeonHole4",
+            "value": 0.26236181127819774,
+            "unit": "ms/iter",
+            "extra": "iterations: 2660\ncpu: 0.26231667593984975 ms\nthreads: 1"
+          },
+          {
+            "name": "BM_UnifromRandom3SatSatInstance",
+            "value": 1.5274572838427403,
+            "unit": "ms/iter",
+            "extra": "iterations: 458\ncpu: 1.5274052096069872 ms\nthreads: 1"
+          },
+          {
+            "name": "BM_UnifromRandom3SatUnsatInstance",
+            "value": 2.0033347507163213,
+            "unit": "ms/iter",
+            "extra": "iterations: 349\ncpu: 2.0031025014326667 ms\nthreads: 1"
           }
         ]
       }
