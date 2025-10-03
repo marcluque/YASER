28911267b45ead7afd81bbc5c341411a0c61bfc1
Marc Luqué <marc.luque@outlook.com>
2025-10-03 23:40:59 +0200

    Revert "Reduce number of allowed learned clauses to avoid bad_alloc for Github Actions"
    This reverts commit cd348f20321dbc9018d7bef680b8192eeb40389d.

--- src/formula.cpp
+++ src/formula.cpp
@@ -21,7 +21,7 @@ Formula::Formula(const std::size_t num_variables, const std::size_t num_clauses,
       m_decision_level(0), m_literals(num_variables * num_clauses), m_literal_ranges(num_clauses), m_assignment_map(num_variables + 1),
       m_variable_assignment_index(num_variables + 1), m_variable_decision_level(num_variables + 1),
       m_unit_clause_map(num_clauses), m_literal_priority((num_variables + 1) * 2),
-      m_clause_priority(num_clauses), m_learned_clause_limit(num_clauses * 2),
+      m_clause_priority(num_clauses), m_learned_clause_limit(num_clauses * 100),
       m_locked_clause_map(num_clauses), m_certificate_output_stream(certificate_path, std::ios::app) {
     DEBUG_LOG("Using certificate path {}", (std::filesystem::temp_directory_path() / "yaser.tmp").string());
     DEBUG_LOG("Number of allowed learned clauses: {}", m_learned_clause_limit);
