07e14a8d299ad1c96c129844a1f7d4ee56fb6a06
Marc Luqué <marc.luque@outlook.com>
2025-10-03 21:48:27 +0200

    Move debug message for certificate path

--- src/formula.cpp
+++ src/formula.cpp
@@ -14,7 +14,6 @@
 
 Formula::Formula(const std::size_t num_variables, const std::size_t num_clauses)
  : Formula(num_variables, num_clauses, std::filesystem::temp_directory_path() / "yaser.tmp") {
-    DEBUG_LOG("Using certificate path {}", (std::filesystem::temp_directory_path() / "yaser.tmp").string());
 }
 
 Formula::Formula(const std::size_t num_variables, const std::size_t num_clauses, const std::filesystem::path& certificate_path)
@@ -24,6 +23,7 @@ Formula::Formula(const std::size_t num_variables, const std::size_t num_clauses,
       m_unit_clause_map(num_clauses), m_literal_priority((num_variables + 1) * 2),
       m_clause_priority(num_clauses), m_learned_clause_limit(num_clauses * 100),
       m_locked_clause_map(num_clauses), m_certificate_output_stream(certificate_path, std::ios::app) {
+    DEBUG_LOG("Using certificate path {}", (std::filesystem::temp_directory_path() / "yaser.tmp").string());
     DEBUG_LOG("Number of allowed learned clauses: {}", m_learned_clause_limit);
 }
 
